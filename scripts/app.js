3371ec78c6b10ff6c02131fddaad6aea0e657da9
Musfirah Waseem <musfirahwaseem0@gmail.com>
2023-04-04 16:33:50 +0500

    :FEAT Add scripts

---
+++ scripts/app.js
@@ -0,0 +1,41 @@
+// Day 4/4/2023
+
+// alert("Hello Musfirah");
+// alert ( "Thanks for your input!" );
+
+// Variables
+// var name ="Musfirah";
+// console.log (name);
+// var firstName ="Musfirah";
+// var lastName ="Muhammad Waseem";
+// var qualification ="BS Space Science";
+// console .log(firstName ,lastName ,qualification);
+// console.log("My name is" ,firstName,"."+ "My Father name is",lastName, "."+"And i done my",qualification,"recently");
+// var Name = prompt("What's Your name?" , "Anas");
+// document.write("Hello " +Name);
+// var lastName =prompt("Your name?","It should be last name.");
+// var cityOfOrigin = "Karachi";
+// var aussieGreeting = "Have a great day!";
+// document.write("Hello ",lastName," Citizen of ",cityOfOrigin , " Dear! ", aussieGreeting );
+
+// Number
+// var originalNum = 23; 
+// var numToBeAdded = 7; 
+// var newNum = originalNum + numToBeAdded;
+// console.log(newNum);
+
+// Math Expressions
+// var add =2+9;
+// console.log(add);
+// var subtract=3-9;
+// console.log(subtract);
+// var multiply = 93*2;
+// console.log(multiply);
+// var divide =151/3;
+// console.log(divide);
+// var power=9**9;
+// console.log(power);
+// var math = 7*6+3-9;
+// console.log(math);
+// var right =(3+8)*(5/9)+(15-99);
+// console.log(right);
